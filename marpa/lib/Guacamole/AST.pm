f2eeb37baf9c7aa28a8c3c1f722def90200a1fad
Sawyer X <xsawyerx@cpan.org>
2020-06-05 23:52:20 +0300

    Clean up AST.pm

--- marpa/lib/Guacamole/AST.pm
+++ marpa/lib/Guacamole/AST.pm
@@ -7,49 +7,6 @@ use Data::Dumper;
 
 our @EXPORT_OK = qw/traverse cleanup/;
 
-# Translate a subtree of nested comma expressions into a List.
-sub fold_comma {
-    my $node = shift;
-    return $node unless ref $node;
-    
-    my ($head, $l, $op, $r) = @$node;
-    return $node unless $head eq "Expression" && defined $op && ($op eq "," || $op eq "=>");
-
-    return [ List => _fold_comma_inner($node) ];
-
-}
-
-sub _fold_comma_inner {
-    my $node = shift;
-    return ($node) unless ref $node;
-
-    my ($head, $l, $op, $r) = @$node;
-    return ($node) unless $head eq "Expression" && defined $op && ($op eq "," || $op eq "=>");
-
-    my @el = _fold_comma_inner($l);
-    my @er = _fold_comma_inner($r);
-
-    return (@el, @er);
-}
-
-# Combine nested Ident nodes.
-sub fold_ident {
-    my $node = shift;
-
-    return $node unless ref $node;
-
-    my ($head, $name, $sep, $rest) = @$node;
-    return $node unless $head eq "Ident";
-
-    my @path = ($name);
-    while (defined $rest && ref $rest) {
-        ($head, $name, $sep, $rest) = @$rest;
-        push @path, $name;
-    }
-
-    return [ Ident => @path ];
-}
-
 # Remove useless text nodes.
 my %_clean_paren_type = (
     "ParenExpr" => [ "(", ")" ],
@@ -75,36 +32,12 @@ sub clean_paren {
     return [ $head, @list ];
 }
 
-# Just for lulz
-sub fold_constants {
-    my $node = shift;
-    return $node unless ref $node;
 
-    my ($head, $l, $op, $r) = @$node;
-    return $node unless $head eq "Expression" && defined $op && ($op eq "+" || $op eq "-" || $op eq "*" || $op eq "/");
 
-    # Fold our arguments first.
-    my $fl = traverse($l, "fold_constants");
-    my $fr = traverse($r, "fold_constants");
 
-    my $ll = _extract_literal($fl) // return $node;
-    my $lr = _extract_literal($fr) // return $node;
 
-    my $expr = "$ll $op $lr";
-    my $result = eval "$expr" // die "Failed to fold expression: $expr: $@";
 
-    return [ Expression => [ Value => [ Literal => $result ]]];
-}
 
-sub _extract_literal {
-    my $node = shift;
-
-    # We don't need no pattern matching...
-    return ref $node && $node->[0] eq "Expression" && ref ($node = $node->[1]) 
-                     && $node->[0] eq "Value" && ref ($node = $node->[1]) 
-                     && $node->[0] eq "Literal" && !ref ($node = $node->[1])
-        ? $node
-        : undef;
 }
 
 sub traverse {
@@ -122,9 +55,7 @@ sub traverse {
 sub cleanup {
     my $node = shift;
 
-    $node = traverse($node, "fold_comma");
-    $node = traverse($node, "fold_ident");
-    $node = traverse($node, "clean_paren");
+    traverse($node, "clean_paren");
 
     return $node;
 }
